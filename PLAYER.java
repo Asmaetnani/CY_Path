87731460c38a4cf1c0567fa7fc35c48ba57545f6
ChaimaRadi1 <130800025+ChaimaRadi1@users.noreply.github.com>
2023-05-25 14:25:34 +0200

    Update PLAYER.java

--- PLAYER.java
+++ PLAYER.java
@@ -1,13 +1,26 @@
+//y line X column 
+
+package cyPath;
+
+import java.util.List;
+
 public class Player extends Pion  {
+	
 	String name;
 	Pion pion;
 	int color; // color is not in the Pion class because the gamer is the one who choose the
 				// color
 	int barriersLeft;
+    private OccupiedCoordinates occupiedCoordinates;
+    
 
+	
+	
 	// constructor
 	public Player(Pion pion,String name, int color, int barriersLeft, int boardSize, int numberOfPlayer, int indexOfPlayer) {
-	    super(pion.getPositionX(),pion.getPositionY());
+		 
+		super(pion.getPositionX(),pion.getPositionY());
+		occupiedCoordinates = new OccupiedCoordinates();
 		this.name = name;
 		this.color = color; // when we will present the game we will indicate that each color is presented
 							// by a number
@@ -51,7 +64,12 @@ public class Player extends Pion  {
 
 		}
 	}
+	
+	
 
+	
+	
+	
 	// getters and setters
 	public String getName() {
 		return this.name;
@@ -85,48 +103,88 @@ public class Player extends Pion  {
 		this.pion = pion;
 	}
 
+	
+	
+	
+	
 	// methods to control the game
 
-	public void makeMove(Coordinate destination) {
+	public void makeMove(Coordinate destination,List<Barrier> listBarriers) {
 	    // Vérifier si la destination est adjacente
+		
 	    if (Adjacent(destination)) {
+	    	
 	        // Vérifier si la destination est déjà occupée
-	        if (!occupied(destination)) {
+	    	
+	    	if (!occupiedCoordinates.isCoordinateOccupied(destination))  {
+	    	       
 	            // Vérifier si le déplacement est horizontal ou vertical
+	    	        
 	            if (respectedMove(pion.getPositionX(), pion.getPositionY(), destination.getPositionX(), destination.getPositionY())) {
 	                pion = new Pion(destination.getPositionX(), destination.getPositionY());
+	                occupiedCoordinates.addCoordinate(destination);
+	                
 	            } else {
-	                // Vérifier si les coordonnées sont dans les limites du plateau
+	                // recuperer les coordonnees du case au mileu du case de pion et la case de destination(apres saut)
 	                int posmidX = (pion.getPositionX() + destination.getPositionX()) / 2;
 	                int posmidY = (pion.getPositionY() + destination.getPositionY()) / 2;
+	                
 	                if (validCoordinate(posmidX, posmidY)) {
-	                    // Vérifier si la case à sauter est vide
-	                    if (!occupied(destination.getPositionX(), destination.getPositionY())) {
+	                	Coordinate middleCoordinate=new Coordinate(posmidX, posmidY);
+	                	
+	                   //verifier que la destination est vide la case du milieu est pleine et pas de barriere entre case du milieu et case d arrivee+
+	                	 boolean barrierExists = Barrier.isBarrier(pion.position, destination, listBarriers);
+	                    if (!occupiedCoordinates.isCoordinateOccupied(destination)&&(!barrierExists)&&occupiedCoordinates.isCoordinateOccupied(middleCoordinate)) {
 	                        pion = new Pion(destination.getPositionX(), destination.getPositionY());
+	                        occupiedCoordinates.addCoordinate(destination); // after verifing the whole conditions we have to mark that this cell is now occupied 
 	                    } else {
-	                        throw new IllegalArgumentException("La case d'arrivée est déjà occupée.");
+	                        throw new IllegalArgumentException("La case d'arrivée est déjà occupée ou case a sauter est vide .");
 	                    }
 	                } else {
-	                    throw new IllegalArgumentException("La case à sauter est vide.");
+	                    throw new IllegalArgumentException("coordonnee invalide");
 	                }
 	            }
 	        } else {
-	            throw new IllegalArgumentException("Coordonnées en dehors du plateau de jeu.");
+	            throw new IllegalArgumentException("Case occupeé.");
 	        }
 	    } else {
 	        throw new IllegalArgumentException("Coordonnées non adjacentes.");
 	    }
 	}
+	
+	
+	
 	public boolean Adjacent(Coordinate obj) {
 		   int xdiff=Math.abs(this.position.getPositionX()-obj.getPositionX());
 		   int ydiff=Math.abs(this.position.getPositionY()-obj.getPositionY());
 		     return (xdiff==1 && ydiff==0)||(xdiff==0 && ydiff==1);
 	    }
-	 //need class Boar
-	    public boolean occupied(Board board) {
-	    	return board.occupied(this);
-}
+	 
+	
+	public boolean respectedMove(int startX, int startY, int destX, int destY) {
+	   // star x and y are the current cell
+		// Vérifier si le mouvement respecte les règles (horizontal ou vertical d'une case)
+
+	    // Vérifier si le mouvement est horizontal (même ligne) 3ndhom same y
+	    if (startY == destY) {
+	        int diffX = Math.abs(destX - startX);
+	        return diffX == 1; // Le mouvement est d'une case horizontalement
 	    }
+	    // Vérifier si le mouvement est vertical (même colonne) same X
+	    else if (startX == destX) {
+	        int diffY = Math.abs(destY - startY);
+	        return diffY == 1; // Le mouvement est d'une case verticalement
+	    }
+
+	    return false; // Le mouvement n'est ni horizontal ni vertical
+	}
+	 public boolean validCoordinate(int ligne, int colonne) {
+	        return ligne >= 0 && ligne < 9 && colonne >= 0 && colonne < 9;
+	 }
+	 
+	 
+	
+}
 
 
 
